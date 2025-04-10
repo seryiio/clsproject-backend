2ccd67aef43c75734059d261694255c07a7fd4ce
seryiio <sergiohuamanch15@gmail.com>
2025-04-09 21:00:06 -0500

    change host

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ import cors from 'cors';
 import velocidadOptimaRoutes from './routes/velocidadOptima.routes.js'
 import codigoOperacionRoutes from './routes/codigoOperacion.routes.js'
 import ordenNavegacionOptimaRoutes from './routes/ordenNavegacionOptima.routes.js'
-import autenticacionCLS from './routes/autenticacion.routes.js'
-import roleCLS from './routes/role.routes.js'
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,11 +16,9 @@ app.use(cors());
 app.use('/api/v1', velocidadOptimaRoutes)
 app.use('/api/v1', codigoOperacionRoutes)
 app.use('/api/v1', ordenNavegacionOptimaRoutes)
-// app.use('/api/v1', autenticacionCLS)
-// app.use('/api/v1', roleCLS)
 
 app.listen(port, () => {
-    console.log(`port running on port http://localhost:${port}`);
+    console.log(`RUNNING ON ${port}`);
 });
 
 export default app;
